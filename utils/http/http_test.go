b774a4eab43c2f9dd9f8ff9ec43c89fdcc8eda39
findcbi <liubiao@robo2025.com>
2018-09-13 16:05:28 +0800

    增加单元测试

---
+++ utils/http/http_test.go
@@ -0,0 +1,20 @@
+package http
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gosharp/config"
+	"gosharp/utils/log"
+	"testing"
+)
+
+func TestHttp(t *testing.T) {
+	asserts := assert.New(t)
+
+	//配置文件初始化
+	config.Init("../../config")
+	//日志初始化
+	log.Init("logs")
+
+	b, data := SendRequest("GET", "https://testapi.robo2025.com/sso/server/verify", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MzY4MjA1MDIsInNpZCI6Ik1UVXpOamd5TURVd01YeEJkMUZCV0VFOVBYeEFtV1ZJaVJrdWtTXzFudlRKNmxEYUhlN0RrcXUycF9XZlc1akJsUjVQenc9PSIsInVpZCI6NDZ9.IJ5_ZZbL_0v_FTBqEHjQgRe9zXBhgH5CNk6ALQb0ujc", nil)
+	asserts.Equal(true, b, "send request error:", data)
+}
